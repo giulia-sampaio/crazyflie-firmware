a4f6104c93f9a1cb41460b3ad4ac114b15badc75
Giulia <giuliacms@al.insper.edu.br>
2023-08-29 08:08:13 -0300

    Drone calibrado ok

--- programs/chapters/ch02.h
+++ programs/chapters/ch02.h
@@ -2,19 +2,16 @@
 #include "mbed.h"
 
 // Define motor as PWM output object
-PwmOut motor(MOTOR1);
+PwmOut motor(MOTOR4);
 
 // Definição das constantes usadas
-float a = 0;
-float b = 0;
+float a = 1.4183e-07;
+float b = 2.2982e-10;
 float c = 0;
-
-// Variável para armazenar PWM usado
-float PWM = 0;
 
 // Converts desired angular velocity (rad/s) to PWM signal (%)
 float control_motor(float omega) {
-  PWM = a * pow(omega, 2) + b * omega + c;
+  float PWM = a * pow(omega, 2) + b * omega + c;
   return PWM;
 }
 
@@ -23,8 +20,8 @@ int main() {
   // Set PWM frequency to 500 Hz
   motor.period(1.0 / 500.0);
   // Turn on motor with 1.000 rad /s for 0.5s
-  motor = control_motor(1000.0);
-  wait(0.5);
+  motor.write(control_motor(1000.0));
+  wait(5);
   // Turn off motor
   motor = 0.0;
   // End of program
