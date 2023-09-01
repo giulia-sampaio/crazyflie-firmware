b55b7f19833776b9f0022f133e3532784d0d576a
Giulia <giuliacms@al.insper.edu.br>
2023-09-01 07:49:55 -0300

    ch02.h modificado para adição de código do capítulo 4

--- programs/chapters/ch02.h
+++ programs/chapters/ch02.h
@@ -2,9 +2,13 @@
 #include "mbed.h"
 
 // Define motor as PWM output object
-PwmOut motor(MOTOR4);
+PwmOut motor1(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
 
-// Definição das constantes usadas
+// Definição dos parâmetros usados para determinar o PWM relativo a cada
+// velocidade
 float a = 1.4183e-07;
 float b = 2.2982e-10;
 float c = 0;
@@ -17,14 +21,107 @@ float control_motor(float omega) {
 
 // Main program
 int main() {
+
   // Set PWM frequency to 500 Hz
-  motor.period(1.0 / 500.0);
-  // Turn on motor with 1.000 rad /s for 0.5s
-  motor.write(control_motor(1000.0));
-  wait(5);
+  //   motor1.period(1.0 / 500.0);
+  //   motor2.period(1.0 / 500.0);
+  //   motor3.period(1.0 / 500.0);
+  //   motor4.period(1.0 / 500.0);
+
+  // Atividades capítulos 2 e 3
+  // Turn on motor with 1.000 rad /s for 5s
+  //   motor1.write(control_motor(1000.0));
+  //   motor2.write(control_motor(1000.0));
+  //   motor3.write(control_motor(1000.0));
+  //   motor4.write(control_motor(1000.0));
+  //   wait(7);
+
   // Turn off motor
-  motor = 0.0;
+  //   motor1 = 0.0;
+  //   motor2 = 0.0;
+  //   motor3 = 0.0;
+  //   motor4 = 0.0;
+  //   wait(5);
+
+  // Turn on motor with 1.200 rad /s for 0.5s
+  //   motor1.write(control_motor(1200.0));
+  //   motor2.write(control_motor(1200.0));
+  //   motor3.write(control_motor(1200.0));
+  //   motor4.write(control_motor(1200.0));
+  //   wait(7);
+
+  // Turn off motor
+  //   motor1 = 0.0;
+  //   motor2 = 0.0;
+  //   motor3 = 0.0;
+  //   motor4 = 0.0;
+  //   wait(5);
+
+  // Turn on motor with 1.400 rad /s for 5s
+  //   motor1.write(control_motor(1400.0));
+  //   motor2.write(control_motor(1400.0));
+  //   motor3.write(control_motor(1400.0));
+  //   motor4.write(control_motor(1400.0));
+  //   wait(7);
+
+  // Turn off motor
+  //   motor1 = 0.0;
+  //   motor2 = 0.0;
+  //   motor3 = 0.0;
+  //   motor4 = 0.0;
+  //   wait(5);
+
+  // Turn on motor with 1.600 rad /s for 5s
+  //   motor1.write(control_motor(1600.0));
+  //   motor2.write(control_motor(1600.0));
+  //   motor3.write(control_motor(1600.0));
+  //   motor4.write(control_motor(1600.0));
+  //   wait(7);
+
+  // Turn off motor
+  //   motor1 = 0.0;
+  //   motor2 = 0.0;
+  //   motor3 = 0.0;
+  //   motor4 = 0.0;
+  //   wait(5);
+
+  // Turn on motor with 1.800 rad /s for 5s
+  //   motor1.write(control_motor(1800.0));
+  //   motor2.write(control_motor(1800.0));
+  //   motor3.write(control_motor(1800.0));
+  //   motor4.write(control_motor(1800.0));
+  //   wait(7);
+
+  // Turn off motor
+  //   motor1 = 0.0;
+  //   motor2 = 0.0;
+  //   motor3 = 0.0;
+  //   motor4 = 0.0;
+  //   wait(5);
+
+  // Turn on motor with 2.000 rad /s for 5s
+  //   motor1.write(control_motor(2000.0));
+  //   motor2.write(control_motor(2000.0));
+  //   motor3.write(control_motor(2000.0));
+  //   motor4.write(control_motor(2000.0));
+  //   wait(7);
+
+  // Atividade capitulo 4:
+  wait(3);
+  motor1.write(control_motor(1000.0));
+  motor2.write(control_motor(2000.0));
+  motor3.write(control_motor(1000.0));
+  motor4.write(control_motor(2000.0));
+  wait(2);
+
+  // Turn off motor
+  motor1 = 0.0;
+  motor2 = 0.0;
+  motor3 = 0.0;
+  motor4 = 0.0;
+
   // End of program
+
   while (true) {
   }
 }
