cabb52c895b7f3b26fdc5ca0d31fe47502857d60
Luis <luisantoniobordignon@gmail.com>
2023-09-14 09:06:29 -0300

    Adicionando mixer

---
+++ programs/chapters/ch09.h
@@ -0,0 +1,17 @@
+#include " crazyflie .h"
+#include " mbed .h"
+
+// Declare mixer
+Mixer mixer;
+
+// Main program
+int main() {
+  // Actuate motor with 50% mg total thrust force (N) and zero torques
+  mixer.actuate(0.5 * m * g, 0, 0, 0);
+  wait(5);
+  // Turn off all motors
+  mixer.actuate(0, 0, 0, 0);
+  // End of program
+  while (true) {
+  }
+}
