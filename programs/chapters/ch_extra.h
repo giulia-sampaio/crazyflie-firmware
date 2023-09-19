7dd25c6f96b1aa1a04e8483a2c6cf1173adff648
Luis <luisantoniobordignon@gmail.com>
2023-09-19 08:25:45 -0300

    Add musica

--- programs/chapters/ch_extra.h
+++ programs/chapters/ch_extra.h
@@ -1,6 +1,13 @@
 #include "mbed.h"
 #include "crazyflie.h"
 
+# define F5 1479.977539
+# define D5 1174.659058
+# define B5 1975.533325
+# define E5 1318.510254
+# define C5 1108.730591
+
+
 // Define motor 1 as PWM output object
 PwmOut motor(MOTOR1);
 
@@ -10,9 +17,8 @@ int main()
     // Turn motor on (20% power) and off every second
     while(true) 
     {
-        motor = 0.2;
-        // wait(0.5);
-        // motor = 0.0;
-        // wait(0.5);
+        motor1.period(1.0/F5);
+        motor1 = 0.2;
+        wait(0.5);
     }
 }
